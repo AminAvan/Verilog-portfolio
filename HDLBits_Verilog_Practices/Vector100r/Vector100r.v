8fd37e46c8314f273b6e222e664f8e389416e9f0
Amin Avan <aminavan.hardware@yahoo.com>
2026-04-02 13:12:03 -0400

    Update with FOR-LOOP definition: https://hdlbits.01xz.net/wiki/Vector100r

--- HDLBits_Verilog_Practices/Vector100r/Vector100r.v
+++ HDLBits_Verilog_Practices/Vector100r/Vector100r.v
@@ -16,3 +16,17 @@ module top_module(
                         in[90], in[91], in[92], in[93], in[94], in[95], in[96], in[97], in[98], in[99]};
 
 endmodule
+
+/*
+module top_module (
+	input [99:0] in,
+	output reg [99:0] out
+);
+	
+	always @(*) begin
+		for (int i=0;i<$bits(out);i++)		// $bits() is a system function that returns the width of a signal.
+			out[i] = in[$bits(out)-i-1];	// $bits(out) is 100 because out is 100 bits wide.
+	end
+	
+endmodule
+*/
