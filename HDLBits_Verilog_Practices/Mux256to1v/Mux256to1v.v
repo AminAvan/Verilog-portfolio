52e50b79f1a90d418cc9245a3f368083f2b4cccc
Amin Avan <aminavan.hardware@yahoo.com>
2026-04-05 21:19:11 -0400

    https://hdlbits.01xz.net/wiki/Mux256to1v

---
+++ HDLBits_Verilog_Practices/Mux256to1v/Mux256to1v.v
@@ -0,0 +1,20 @@
+`default_nettype none
+module top_module(
+    input wire [1023:0] in,
+    input wire [7:0] sel,
+    output wire [3:0] out
+);  
+    reg [3:0] reg_out;
+    integer i;
+
+    always @(*) begin
+        for (i = 0; i < 256; i = i + 1) begin
+            if (sel == i) begin
+                reg_out[3:0] = in[i*4 +: 4];
+            end
+        end
+    end
+
+    assign out[3:0] = reg_out[3:0];
+
+endmodule
