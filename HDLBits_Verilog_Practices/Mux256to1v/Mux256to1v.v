7b8434bc6520c0e90265e248b7b1aef068a56d50
Amin Avan <aminavan.hardware@yahoo.com>
2026-04-05 21:22:35 -0400

    https://hdlbits.01xz.net/wiki/Mux256to1v

--- HDLBits_Verilog_Practices/Mux256to1v/Mux256to1v.v
+++ HDLBits_Verilog_Practices/Mux256to1v/Mux256to1v.v
@@ -5,16 +5,25 @@ module top_module(
     output wire [3:0] out
 );  
     reg [3:0] reg_out;
-    integer i;
+    
+    //// First Approach to solve
+    // integer i;
 
+    // always @(*) begin
+    //     for (i = 0; i < 256; i = i + 1) begin
+    //         if (sel == i) begin
+    //             reg_out[3:0] = in[i*4 +: 4];
+    //         end
+    //     end
+    // end
+
+    //// Second approach to solve
     always @(*) begin
-        for (i = 0; i < 256; i = i + 1) begin
-            if (sel == i) begin
-                reg_out[3:0] = in[i*4 +: 4];
-            end
-        end
+        reg_out[3:0] = in[sel*4 +: 4];
     end
+    
 
+    // Need to be used for both approaches
     assign out[3:0] = reg_out[3:0];
 
 endmodule
